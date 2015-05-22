e0362fed832f7c234832921d14381c9772abc0e1
Johnson Zhou <johsonchou@gmail.com>
2015-05-22 13:45:50 -0700

    building 2

---
+++ polyConnect/polyConnect/FloorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FloorTableViewCell.swift
+//  polyConnect
+//
+//  Created by Johnson Zhou on 5/22/15.
+//  Copyright (c) 2015 Johnson Zhou. All rights reserved.
+//
+
+import UIKit
+
+class FloorTableViewCell: UITableViewCell {
+    @IBOutlet weak var floorLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ polyConnect/polyConnect/FloorTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  FloorTableViewController.swift
+//  polyConnect
+//
+//  Created by Johnson Zhou on 5/22/15.
+//  Copyright (c) 2015 Johnson Zhou. All rights reserved.
+//
+
+import UIKit
+
+class FloorTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 80
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    var token: dispatch_once_t = 0
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        dispatch_once(&token) {
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 5
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("floorCell", forIndexPath: indexPath) as! FloorTableViewCell
+
+        // Configure the cell...
+        cell.backgroundColor = UIColor(netHex: 0x3f6c55)
+        switch indexPath.row {
+        case 0:
+            cell.floorLabel.text = "FIRST FLOOR"
+        case 1:
+            cell.floorLabel.text = "SECOND FLOOR"
+        case 2:
+            cell.floorLabel.text = "THIRD FLOOR"
+        case 3:
+            cell.floorLabel.text = "FOURTH FLOOR"
+        case 4:
+            cell.floorLabel.text = "FIFTH FLOOR"
+        default:
+            cell.floorLabel.text = "ERROR"
+            
+        }
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        cell.layoutIfNeeded()
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
